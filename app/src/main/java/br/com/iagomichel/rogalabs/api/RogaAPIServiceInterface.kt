ee1537e70a72b1820b9262f6f073a954adbff3e6
IagoMichel <iago.michelazevedo@gmail.com>
2022-05-28 23:41:26 -0300

    Adicionado chamada de api POSTS

--- app/src/main/java/br/com/iagomichel/rogalabs/api/RogaAPIServiceInterface.kt
+++ app/src/main/java/br/com/iagomichel/rogalabs/api/RogaAPIServiceInterface.kt
@@ -1,10 +1,15 @@
 package br.com.iagomichel.rogalabs.api
 
 import br.com.iagomichel.rogalabs.BuildConfig
+import br.com.iagomichel.rogalabs.models.Post
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 interface RogaAPIServiceInterface {
+
+    @GET("/posts")
+    suspend fun fetchPosts(): List<Post>
 
     companion object Factory {
         fun create() =
