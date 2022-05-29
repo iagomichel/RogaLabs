6f9c7e706c5aa05cbe72703c3266fe5d7c9b94c7
IagoMichel <iago.michelazevedo@gmail.com>
2022-05-28 22:14:52 -0300

    Adicionado base para retrofit.

--- app/src/main/java/br/com/iagomichel/rogalabs/api/RogaAPIServiceInterface.kt
+++ app/src/main/java/br/com/iagomichel/rogalabs/api/RogaAPIServiceInterface.kt
@@ -7,14 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 interface RogaAPIServiceInterface {
 
     companion object Factory {
-        fun create(): RogaAPIServiceInterface {
-            val retrofit =
-                Retrofit.Builder()
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(BuildConfig.API_URL)
-                    .build()
-
-            return retrofit.create(RogaAPIServiceInterface::class.java)
-        }
+        fun create() =
+            Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BuildConfig.API_URL)
+                .build()
+                .create(RogaAPIServiceInterface::class.java)
     }
 }
